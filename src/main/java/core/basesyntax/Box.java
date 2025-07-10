13bb3f2785790f77ab52a328e3a95dfd6fc074e5
Andrei Filonenko <a1.sankai@gmail.com>
2025-07-10 15:34:53 +0300

    implementetion methods by generic array

---
+++ src/main/java/core/basesyntax/Box.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class Box {
+
+    public Box() {
+
+    }
+
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,60 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAXSIZE = 10;
+    private static int maxIndex = 0;
+
+    private Object[] keys = new Object[MAXSIZE];
+    private Object[] values = new Object[MAXSIZE];
+
+    {
+        maxIndex = 0;
+    }
+
+    //public StorageImpl() {
+    //}
+
     @Override
     public void put(K key, V value) {
+
+        if (maxIndex + 1 == MAXSIZE) {
+            throw new ArrayIndexOutOfBoundsException("Index out of bounds!");
+        }
+
+        if (maxIndex > 0) {
+            for (int i = 0; i < maxIndex; i++) {
+                if (keys[i] != null && keys[i].equals(key) || (keys[i] == null && key == null)) {
+                    values[i] = value;
+                    return;
+                }
+            }
+        }
+
+        keys[maxIndex] = key;
+        values[maxIndex] = value;
+
+        if (maxIndex + 1 > MAXSIZE) {
+            return;
+        }
+        maxIndex++;
+
     }
 
     @Override
     public V get(K key) {
+
+        for (int i = 0; i < maxIndex; i++) {
+            if ((keys[i] != null && keys[i].equals(key)) || (keys[i] == null && key == null)) {
+                return (V) values[i];
+            }
+        }
+
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return maxIndex;
     }
 }
